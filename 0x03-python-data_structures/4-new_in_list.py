e5c53a9780fb21c5f1d0e67afe4af5d9f8918f3c
ElSanzioKE <lesanjorelly@gmail.com>
2023-12-04 07:30:36 +0000

    new list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, new_element):
+
+    """replaces an element in a specific location"""
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+
+    new_list = my_list.copy()
+    new_list[idx] =new_element
+    return new_list
+
