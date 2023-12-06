5861293d4d2843dd59cf71b2116635a4d3178d44
ElSanzioKE <lesanjorelly@gmail.com>
2023-12-06 16:47:19 +0000

    REuby

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    
+    """ returns a set of all elements present in only one set."""
+
+    result_set = set()
+    for elem in set_1:
+        if elem not in set_2:
+            result_set.add(elem)
+    for elem in set_2:
+        if elem not in set_1:
+            result_set.add(elem)
+
+    return result_set
