cc2102e170f5bc1e1f9bca0daf6915261a4d4e6a
ElsanzioKE <lesanjorelly@gmail.com>
2024-03-16 08:45:30 +0300

    states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python3
+"""
+script that lists states in ascending order in id
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <usernme> <password> <database>" .format(sys.argv[0]))
-        sys.exit(1)
-
+    #arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-
-    try:
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=username,
-                passwd=password,
-                db=database
-                )
-    except MySQLdb.Error as e:
-        print("Error connecting to MysQL:", e)
-        sys.exit(10)
-
-    cursor = db.cursor()
-
-    try:
-        cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-        states = cursor.fetchall()
-    except MySQLdb.error as e:
-        print("Error executing query:", e)
-        db.clode()
-        sys.exit(1)
-
-    for state in states:
-        print(state)
-
+    # connect to the sql server
+    connection = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    #create a cursor element
+    cursor = connection.cursor()
+    rows = cursor.fetchall()
+    #dispaly results
+    for row in rows:
+        print(row)
+    # close connection
     cursor.close()
-    db.close()
+    connection.close()
+
