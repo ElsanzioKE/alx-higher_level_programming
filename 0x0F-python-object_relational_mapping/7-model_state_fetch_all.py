04fd87bb6d46696bf3dd1485139b203a76e719ab
ElsanzioKE <lesanjorelly@gmail.com>
2024-03-17 12:53:34 +0300

    Auto commit at Sun Mar 17 12:53:34 EAT 2024

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,6 +7,10 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("usage: {} ,use< username> <password> <database>". format(sys.argv[0]))
+        sys.exit(1)
     # Create engine to connect to MySQL database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
